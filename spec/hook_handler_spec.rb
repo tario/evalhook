9948020ec35f800abb82f96ac7a08190d30c73e1
Dario <darios3@gmail.com>
2010-12-12 14:21:35 -0300

    added generic it for eval of expresions

--- spec/hook_handler_spec.rb
+++ spec/hook_handler_spec.rb
@@ -2,14 +2,25 @@ require "rubygems"
 require "evalhook"
 
 describe EvalHook::HookHandler, "hook handler" do
-     it "should throw exception when call evalhook with no parameters" do
-		hook_handler = EvalHook::HookHandler.new
+   it "should throw exception when call evalhook with no parameters" do
+  		hook_handler = EvalHook::HookHandler.new
 
+  		lambda {
+  			hook_handler.evalhook
+  		}.should raise_error(ArgumentError)
+   end
 
+  exprlist = ["1+1", "[1,2,3].map{|x| x*2}"]
 
-		lambda {
-			hook_handler.evalhook
-		}.should raise_error(ArgumentError)
-     end
+  exprlist.each do |expr|
+    it "should eval expresion '#{expr}' same as eval" do
+      hook_handler = EvalHook::HookHandler.new
+
+      expected = eval(expr)
+      hook_handler.evalhook(expr).should be == expected
+
+    end
+  end
+
 end
 
