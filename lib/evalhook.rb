739e8ae737fb07662075ce0222ac5de22a4614a4
Dario <darios3@gmail.com>
2010-12-25 15:48:22 -0300

    added empty class MultiHookHandler extending HookHandler, +6 spec pass

--- lib/evalhook.rb
+++ lib/evalhook.rb
@@ -21,6 +21,7 @@ along with evalhook.  if not, see <http://www.gnu.org/licenses/>.
 require "evalhook"
 require "evalhook_base"
 require "evalhook/redirect_helper"
+require "evalhook/multi_hook_handler"
 
 
 class Object
