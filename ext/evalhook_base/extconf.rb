ca0916924005d1f32d48a010468a08cd9c4931e3
Dario <darios3@gmail.com>
2010-12-30 22:14:15 -0300

    added macro define for RUBY1_9 and RUBY1_8

--- ext/evalhook_base/extconf.rb
+++ ext/evalhook_base/extconf.rb
@@ -1,6 +1,19 @@
 require 'mkmf'
 dir_config('evalhook_base')
 CONFIG['CC'] = 'gcc'
+
+ruby_version = Config::CONFIG["ruby_version"]
+ruby_version = ruby_version.split(".")[0..1].join(".")
+
+if ruby_version == "1.8"
+	$CFLAGS = $CFLAGS + " -DRUBY1_8"
+elsif ruby_version == "1.9"
+	$CFLAGS = $CFLAGS + " -DRUBY1_9"
+else
+	print "ERROR: unknown ruby version: #{ruby_version}\n"
+	print "try passing the rubyversion by argument (1.8 or 1.9)\n"
+end
+
 create_makefile('evalhook_base')
 
 
