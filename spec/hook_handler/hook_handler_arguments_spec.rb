b00fc8e358f0daef9a9be028f5ef2da22f0296f5
Dario <darios3@gmail.com>
2011-01-10 23:05:18 -0300

    added argument with bug (fail)

---
+++ spec/hook_handler/hook_handler_arguments_spec.rb
@@ -0,0 +1,15 @@
+require "rubygems"
+require "evalhook"
+
+describe EvalHook::HookHandler, "hook handler" do
+
+  it "should accept specified source name" do
+    hh = EvalHook::HookHandler.new
+
+    begin
+      hh.evalhook("raise '0'",binding, "sourcename", 1)
+    rescue Exception => e
+      e.backtrace.join.include? ("sourcename").should be == true
+    end
+  end
+end
