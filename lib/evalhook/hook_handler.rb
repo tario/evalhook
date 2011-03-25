c768a2fb0be084fa47b95fb895e03d8ee159dd40
Dario <darios3@gmail.com>
2011-03-25 12:01:59 -0300

    created separated rb for HookHandler class and other classes and methods defined in the C extension

---
+++ lib/evalhook/hook_handler.rb
@@ -0,0 +1,29 @@
+=begin
+
+This file is part of the evalhook project, http://github.com/tario/evalhook
+
+Copyright (c) 2010 Roberto Dario Seminara <robertodarioseminara@gmail.com>
+
+evalhook is free software: you can redistribute it and/or modify
+it under the terms of the gnu general public license as published by
+the free software foundation, either version 3 of the license, or
+(at your option) any later version.
+
+evalhook is distributed in the hope that it will be useful,
+but without any warranty; without even the implied warranty of
+merchantability or fitness for a particular purpose.  see the
+gnu general public license for more details.
+
+you should have received a copy of the gnu general public license
+along with evalhook.  if not, see <http://www.gnu.org/licenses/>.
+
+=end
+
+module EvalHook
+  def self.validate_syntax
+   raise "999"
+  end
+
+  class HookHandler
+  end
+end
