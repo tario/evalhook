1ec051376087e941018d27d31368c65319879c3d
Dario <darios3@gmail.com>
2011-05-24 20:04:57 -0300

    +1 test pass: fixed expectation for handle_const and handle_colon2 on test

--- spec/hook_handler/hook_handler_hook_spec.rb
+++ spec/hook_handler/hook_handler_hook_spec.rb
@@ -89,8 +89,13 @@ describe EvalHook::HookHandler, "hook handler hooks" do
   it "should intercept nested constant access" do
     hh = EvalHook::HookHandler.new
 
-    hh.should_receive(:handle_const).once.with("TestModule321")
-    hh.should_receive(:handle_colon2).once.with(TestModule321,"B")
+    hh.should_receive(:handle_const).once.with("TestModule321") {
+	RedirectHelper::Value.new(TestModule321)
+    }
+    
+    hh.should_receive(:handle_colon2).once.with(TestModule321,"B") {
+	RedirectHelper::Value.new(0)
+    }
 
     hh.evalhook("TestModule321::B")
   end
