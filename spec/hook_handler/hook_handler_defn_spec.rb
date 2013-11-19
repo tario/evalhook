30dde59df44b1e6e806ec637485e845706de9490
Dario Seminara <darios3@gmail.com>
2013-11-19 19:22:54 -0300

    +1 failing test: singleton method defined with multiple lines

--- spec/hook_handler/hook_handler_defn_spec.rb
+++ spec/hook_handler/hook_handler_defn_spec.rb
@@ -22,4 +22,23 @@ describe EvalHook::HookHandler, "hook handler defn" do
     foo(xxz)
   end
 
+  it "should execute two lines on defs" do
+    hook_handler = EvalHook::HookHandler.new
+
+    hook_handler.evalhook("
+      def set_foo(obj)
+        def obj.foo(x)
+          x.bar
+          x.bar
+        end
+      end", binding)
+
+    obj = Object.new
+    set_foo(obj)
+
+    xxz = XXZ.new
+    xxz.should_receive(:bar).twice
+    obj.foo(xxz)
+  end
+
 end
